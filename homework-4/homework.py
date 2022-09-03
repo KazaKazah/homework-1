99f7221fd051091486d23946d222d9ab701f4000
Asa <elt.8155@gmail.com>
2022-09-03 13:46:04 +0600

    12

---
+++ homework-4/homework.py
@@ -0,0 +1,22 @@
+# Задача 1: Написать собственное исключение (свой класс) 'AuthError' raise ->
+# ArifmeticError (AuthError)
+
+# class AuthError:
+#     try:
+#         filepath = 'test_file.txt'
+#     with open(filepath, 'r') as fio:
+#         result = fio.readlines()
+#     if not result:
+#         raise Exception("File is empty")
+
+
+# Задача 2: написать декоратор, который, в случае отсутствия данных(внутри request_from_user)
+# от авторизации возбуждает исключение 'AuthError'
+
+
+
+# Задача 3: в случае успеха вернуть Объект(класс) 'RequestManager' + 2 метода для валидации
+
+
+
+# Задача 4: Реализовать «класс-вспомогашку(Help)», в котором реализовать минимум 3 метода которые Вам понадобятся в будущем. А также сохранить константу Pi как свойство класса.
